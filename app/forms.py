c210987b2783cf6a29dd66d0f192d246c5759e07
isaac <isaac.khader@gmail.com>
2019-06-22 21:33:22 -0400

    all the basic fetures are working, but the layout needs to be heavily cleaned up

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectMultipleField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo,Length
+from wtforms import StringField, SelectMultipleField, SubmitField, widgets
+from wtforms.validators import ValidationError, DataRequired, Length
+
+class MultiCheckboxField(SelectMultipleField):
+    """
+    A multiple-select, except displays a list of checkboxes.
+
+    Iterating the field will produce subfields, allowing custom rendering of
+    the enclosed checkbox fields.
+    """
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
 
 
 class SelectionForm(FlaskForm):
-    ticker = StringField('Ticker', validators = [DataRequired()])
-    group_id = SelectMultipleField(u'Group', choices=[('a', 'b'), ('c', 'd')])
+    default_fields  = [('open', 'open'),
+                        ('high', 'high'),
+                        ('low', 'low'),
+                        ('close', 'close'),
+                        ('volume', 'volume'),
+                        ('ex-dividend', 'ex-dividend'),
+                        ('split_ratio', 'split_ratio'),
+                        ('adj_open', 'adj_open'),
+                        ('adj_high', 'adj_high'),
+                        ('adj_low', 'adj_low'),
+                        ('adj_close', 'adj_close'),
+                        ('adj_volume', 'adj_volume')]
+    ticker = StringField('Ticker', validators = [DataRequired(), Length(max = 5)])
+    plot_choices = MultiCheckboxField(u'Plot Values', choices=default_fields, default = 'open')
+    options = MultiCheckboxField(u'Additional Options', choices=[('head', 'Show Table Head'),
+                        ('stats', 'Show Stats')])
     submit = SubmitField('Do it')
 
--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from app.forms import SelectionForm
 from app.ticker_processing import ticker_df
 from bokeh.embed import components
 from bokeh.plotting import figure
+import pandas as pd
 
 
 
@@ -14,24 +15,34 @@ from bokeh.plotting import figure
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     form = SelectionForm()
-    raw_table = ""
+    head_prev, summary = "", ""
     script =""
     div = ""
     if form.validate_on_submit(): # false for a GET request
         ticker = form.ticker.data
         stock = ticker_df(ticker)
-        raw_table = stock.head().to_html()
+        stock = stock[stock.date>= stock.date.max()-pd.Timedelta(days=300)]  # could also request less data... should probably cache this so we don't make an api call every time
+        print(form.options.data)
+     
+        if stock.size > 0 and form.plot_choices.data: # valid ticker returns a table
+            head_prev = stock.head().to_html()
+            summary = stock.describe().to_html()
+             # create a new plot with a title and axis labels
+            p = figure(title="Stock Market Data", x_axis_type='datetime',x_axis_label='x', y_axis_label='y')
+            
+            for data in form.plot_choices.data:
+                print(data)
+                # add a line renderer with legend and line thickness
+                p.line(stock.date,stock[data], legend="Temp.", line_width=2)
 
-        # create a new plot with a title and axis labels
-        p = figure(title="simple line example", x_axis_type='datetime',x_axis_label='x', y_axis_label='y')
-
-        # add a line renderer with legend and line thickness
-        p.line(stock.date,stock.open, legend="Temp.", line_width=2)
-
-        # Embed plot into HTML via Flask Render
-        script, div = components(p)
+            # Embed plot into HTML via Flask Render
+            script, div = components(p)
+        elif stock.size == 0:
+            form.ticker.errors.append("No valid ticker found with that name!")
+        else: 
+            form.plot_choices.errors.append("Please select at least one thing to plot.")
 
     return render_template('index.html', title='Home Page', form=form, 
-                        raw_table = raw_table,script=script, div=div)
+                        head_prev = head_prev,summary = summary,script=script, div=div)
 
 
